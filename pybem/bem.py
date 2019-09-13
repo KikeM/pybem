682e35c5beb444c16017b7b9739d4b4c3d4e43d0
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2019-09-13 19:01:40 +0200

    ENH: included implicit Euler integration for loads.

--- pybem/bem.py
+++ pybem/bem.py
@@ -18,10 +18,19 @@ class BladeElementMethod():
         self.propeller = None
 
         self.solidity = 0.5
-        self.F = 1
+        self.F        = 1
 
         self.beta = None
-        self.r = None
+        self.r    = None
+        self.dr   = None
+
+        self.T = None
+        self.Q = None
+
+        self.T_hat = None
+        self.Q_hat = None
+
+        self.q_inf = None
 
     def load_airfoil(self, alpha, polar_cl, polar_cd):
 
@@ -91,6 +100,88 @@ class BladeElementMethod():
             sol = np.array(np.nan)
 
         return sol.item()
+
+    def compute_loads(self, dr = 0.05):
+        """
+        Compute blade loads.
+
+        Parameters
+        ----------
+        dr: float
+            Spacing between stations.
+
+        Returns
+        -------
+        tuple: thrust, torque
+
+        """
+        from math import pi
+
+        _r_hub = self.propeller.r_hub
+        _r_tip = self.propeller.r_tip
+        
+        N  = np.floor((_r_tip - _r_hub) / dr)
+        
+        F = self.F
+        omega = self.flight.omega
+
+        print(f"Using {N} stations")
+
+        T_hat = [0.0]
+        Q_hat = [0.0]
+
+        # TODO: create property
+        q_inf = 4 * pi * self.flight.atmosphere.rho * (self.flight.v)**2.0
+
+        r_space = np.linspace(_r_hub, _r_tip, N)
+
+        idx = 0
+        for r in r_space[1:]:
+            
+            # Compute induction angle
+            phi = self.compute_inflow_angle(r)
+            
+            # Compute induction coefficients
+            axi = self.induction_axial(phi, self.beta)
+            tng = self.induction_tangential(phi, self.beta)
+            
+            # Compute forcing terms
+            F_T = (r+dr)**1.0 * (1 + axi) * axi * F
+            F_Q = (r+dr)**3.0 * (1 + axi) * tng * F
+
+            T_hat.append(T_hat[idx] + dr * F_T)
+            Q_hat.append(Q_hat[idx] + dr * F_Q)
+            
+            idx +=1
+
+        T_hat = np.array(T_hat)
+        Q_hat = np.array(Q_hat)
+            
+        # Give proper dimensions
+        T = np.array(T_hat)
+        Q = np.array(Q_hat)
+
+        T *= q_inf
+        Q *= q_inf * omega
+
+        self.T = T
+        self.Q = Q
+
+        self.T_hat = T_hat
+        self.Q_hat = Q_hat
+
+        self.dr = dr
+        self.N  = N
+        self.q_inf = q_inf
+        
+        result = dict()
+        result['r'] = r_space
+        result['T'] = T
+        result['Q'] = Q
+        result['T_hat'] = T_hat
+        result['Q_hat'] = Q_hat
+
+        return result
 
     def _residual(self, phi):
 
