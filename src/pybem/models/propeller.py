b2bd2464b64af0b139abea709bce614bd503af1c
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2020-09-20 10:48:13 +0200

    ENH, TST: `Propeller` class updated with a new `Section` object

--- src/pybem/models/propeller.py
+++ src/pybem/models/propeller.py
@@ -1,4 +1,93 @@
+from bisect import bisect_left
+from math import pi
+
 from scipy.interpolate import interp1d
+
+
+class Section:
+    """Propeller blade section.
+
+    This class is used by the `Propeller` class, to build the distribution of
+    chord, twist and solidity across the blade.
+
+    Attributes
+    ----------
+    r : float
+        Dimensionless radius.
+    beta : float
+        Geometrical twist.
+    chord : float
+        Airfoil section chord.
+    solidity : float
+    airfoil : AirfoilBase-like object
+    """
+
+    def __init__(self, r, beta, chord, airfoil, name=None):
+
+        self.r = r
+        self.beta = beta
+        self.chord = chord
+        self.airfoil = airfoil
+        self.name = name
+
+    def __repr__(self):
+        return (
+            f"Section {self.name} (r = {self.r}, c = {self.chord}, beta = {self.beta})"
+        )
+
+    def cl(self, alpha):
+        """Section lift coefficient.
+
+        Parameters
+        ----------
+        alpha : float
+
+        Returns
+        -------
+        cl : float
+        """
+
+        _cl = self.airfoil.compute_cl(alpha=alpha)
+
+        return _cl
+
+    def cd(self, alpha):
+        """Section drag coefficient.
+
+        Parameters
+        ----------
+        alpha : float
+
+        Returns
+        -------
+        cd : float
+        """
+
+        _cd = self.airfoil.compute_cd(alpha=alpha)
+
+        return _cd
+
+    def solidity(self, B, R):
+        """Section solidity.
+
+        Parameters
+        ----------
+        B : int
+            Number of blades in the propeller.
+        R : float
+            Propeller radius.
+
+        Returns
+        -------
+        sigma : float
+        """
+
+        NUM = B * self.chord
+        DEN = 2.0 * pi * R
+
+        sigma = NUM / DEN
+
+        return sigma
 
 
 class Propeller:
@@ -6,67 +95,145 @@ class Propeller:
 
     Parameters
     ----------
-    r_hub : float
-    r_tip : float
-    r_dist : array-like of floats
-    beta_dist : array-like of floats
-    chord_dist : array-like of floats
+    B : int
+    sections : list of Sections-like objects
     """
 
-    def __init__(self, r_hub, r_tip, r_dist, beta_dist, chord_dist):
+    def __init__(self, B, sections):
+
+        self.B = B
 
         # Get blade bounds
-        self.r_hub = r_hub
-        self.r_tip = r_tip
+        self.R = max(section.r for section in sections)
+        self.R_hub = min(section.r for section in sections)
 
-        # Get airfoil properties
-        self.dist_twist = beta_dist
-        self.dist_chord = chord_dist
-        self.dist_r = r_dist
+        # Sort sections by ascending order of radius
+        _sections = sorted(sections, key=lambda section: section.r)
 
-        self.interpolant_twist = interp1d(r_dist, beta_dist)
-        self.interpolant_chord = interp1d(r_dist, chord_dist)
+        self.sections = []
+        dist_r = []
+        dist_beta = []
+        dist_chord = []
+        for section in _sections:
 
-        self.r = None
-        self._beta = None
-        self._chord = None
+            # Make radius dimensionless
+            section.r = section.r / self.R
 
-    def get_beta(self, r):
-        """Airfoil twist at location r.
+            dist_r.append(section.r)
+            dist_beta.append(section.beta)
+            dist_chord.append(section.chord)
 
-        Parameters
-        ----------
-        r: float
-            Location based on r_loc.
+            # Save
+            self.sections.append(section)
 
-        Returns
-        -------
-        beta: float
-        """
-        self.r = r
+        self._interpolant_chord = interp1d(dist_r, dist_chord)
+        self._interpolant_beta = interp1d(dist_r, dist_beta)
 
-        _beta = self.interpolant_twist(r).item()
-
-        self._beta = _beta
-
-        return _beta
-
-    def get_chord(self, r):
-        """Airfoil chord at location r.
+    def _find_bracket(self, r):
+        """For a given dimensionless location `r`, find the
+        two surrounding airfoil Sections.
 
         Parameters
         ----------
-        r: float
-            Location based on r_loc.
+        r : float
+            Dimensionless radius.
 
         Returns
         -------
-        chord: float
+        left : Section-like object.
+        right : Section-like object.
         """
-        self.r = r
 
-        _chord = self.interpolant_chord(r).item()
+        _radius = [section.r for section in self.sections]
 
-        self._chord = _chord
+        idx = bisect_left(_radius, r)
 
-        return _chord
+        left = self.sections[idx - 1]
+        right = self.sections[idx]
+
+        return left, right
+
+    def compute_chord(self, r):
+        return self._interpolant_chord(r).item()
+
+    def compute_beta(self, r):
+        return self._interpolant_beta(r).item()
+
+    def compute_solidity(self, r):
+        """Compute section solidity via interpolation between sections.
+
+        Parameters
+        ----------
+        r : float
+
+        Returns
+        -------
+        sgima : float
+        """
+
+        left, right = self._find_bracket(r)
+
+        radii = [left.r, right.r]
+        solidities = [
+            left.solidity(B=self.B, R=self.R),
+            right.solidity(B=self.B, R=self.R),
+        ]
+        f = interp1d(radii, solidities)
+
+        sigma = f(r)
+
+        return sigma
+
+    def compute_cl(self, r, alpha):
+        """Compute section lift coefficient via interpolation between sections.
+
+        Parameters
+        ----------
+        r : float
+        alpha : float
+
+        Returns
+        -------
+        cl : float
+        """
+
+        left, right = self._find_bracket(r)
+
+        radii = [left.r, right.r]
+        coeffs = [
+            left.cl(alpha=alpha),
+            right.cl(alpha=alpha),
+        ]
+
+        f = interp1d(radii, coeffs)
+
+        cl = f(r)
+
+        return cl
+
+    def compute_cd(self, r, alpha):
+        """Compute section drag coefficient via interpolation between sections.
+
+        Parameters
+        ----------
+        r : float
+        alpha : float
+
+        Returns
+        -------
+        cd : float
+        """
+
+        left, right = self._find_bracket(r)
+
+        radii = [left.r, right.r]
+        coeffs = [
+            left.cd(alpha=alpha),
+            right.cd(alpha=alpha),
+        ]
+
+        f = interp1d(radii, coeffs)
+
+        cd = f(r)
+
+        return cd
--- tests/models/test_propeller.py
+++ tests/models/test_propeller.py
@@ -1,10 +1,82 @@
+from math import pi
+
 import pytest
+from numpy.testing import assert_allclose
 
-from pybem import Propeller
+from pybem.models.propeller import Propeller, Section
 
 
-@pytest.mark.xfail(
-    raises=NotImplementedError, reason="Propeller definition needs to be updated."
-)
-def test_propeller():
-    raise NotImplementedError
+@pytest.fixture
+def propeller():
+    sections = [
+        Section(name="Hub", r=0.3, beta=60, chord=0.2, airfoil=None),
+        Section(name="Middle", r=0.6, beta=45, chord=0.35, airfoil=None),
+        Section(name="Tip", r=1.2, beta=30, chord=0.5, airfoil=None),
+    ]
+
+    B = 6
+
+    propeller = Propeller(B=B, sections=sections)
+
+    return propeller
+
+
+def test_propeller_init_radius(propeller):
+
+    result = [propeller.R_hub, propeller.R]
+    expected = [0.3, 1.2]
+
+    assert_allclose(expected, result)
+
+
+def test_propeller_compute_chord(propeller):
+
+    result = propeller.compute_chord(0.9)
+    expected = 0.47
+
+    assert_allclose(expected, result)
+
+
+def test_propeller_compute_beta(propeller):
+
+    result = propeller.compute_beta(0.9)
+    expected = 33.0
+
+    assert_allclose(expected, result)
+
+
+def test_propeller_find_bracket_left(propeller):
+
+    result_sections = propeller._find_bracket(0.3)
+
+    expected_sections = (propeller.sections[0], propeller.sections[1])
+
+    assert expected_sections == result_sections
+
+
+def test_propeller_find_bracket_right(propeller):
+
+    result_sections = propeller._find_bracket(0.9)
+
+    expected_sections = (propeller.sections[1], propeller.sections[2])
+
+    assert expected_sections == result_sections
+
+
+def test_propeller_solidity(propeller):
+
+    result_sigma = propeller.compute_solidity(0.9)
+
+    expected_sigma = 0.37401411626595404
+
+    assert_allclose(expected_sigma, result_sigma)
+
+
+def test_section_solidity():
+
+    section = Section(r=1.0, beta=0, chord=1.0, airfoil=None)
+
+    result = section.solidity(1, 1.0)
+    expected = 1.0 / (2.0 * pi)
+
+    assert_allclose(expected, result)
